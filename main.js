540ff96f5812aaef5128e7605a84ed34919e2f2c
ShreyasJ1 <109956165+ShreyasJ1@users.noreply.github.com>
2023-07-29 15:09:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,50 @@
 harry_potter = "";
 peter_pan = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
-   harry_potter = loadSound("HarryPotter.mp3");
-   peter_pan = loadSound("PeterPan.mp3");
+    harry_potter = loadSound("HarryPotter.mp3");
+    peter_pan = loadSound("PeterPan.mp3");
 }
 
 function setup() {
-    canvas = createCanvas(500,500);
-    canvas.position(700,250);
-    
+    canvas = createCanvas(500, 500);
+    canvas.position(700, 250);
+
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
-      image(video, 0, 0, 600, 500);
+    image(video, 0, 0, 600, 500);
 }
 
 function play() {
     song.play();
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialised");
+
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + " Left Wrist Y = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + "Right Wrist Y = " + rightWristY);
+        
+    }
 }
